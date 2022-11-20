fcbc962f951ecbd6d8ffdc34cd33604dcee31b96
namnc <namnche141707@fpt.edu.vn>
2022-11-20 22:40:43 +0700

    Book&Post

--- BTP_API/Controllers/PostController.cs
+++ BTP_API/Controllers/PostController.cs
@@ -17,11 +17,7 @@
             try
             {
                 var apiResponse = await _postRepository.get3PostAsync();
-                if (apiResponse.NumberOfRecords != 0)
-                {
                     return Ok(apiResponse);
-                }
-                return NotFound(apiResponse);
             }
             catch
             {
@@ -35,11 +31,7 @@
             try
             {
                 var apiResponse = await _postRepository.getAllPostAsync(page);
-                if (apiResponse.NumberOfRecords != 0)
-                {
                     return Ok(apiResponse);
-                }
-                return NotFound(apiResponse);
             }
             catch
             {
@@ -53,11 +45,7 @@
             try
             {
                 var apiResponse = await _postRepository.getPostByIdAsync(id);
-                if (apiResponse.NumberOfRecords != 0)
-                {
                     return Ok(apiResponse);
-                }
-                return NotFound(apiResponse);
             }
             catch
             {
@@ -71,11 +59,7 @@
             try
             {
                 var apiResponse = await _postRepository.getCommentInPostAsync(id, page);
-                if (apiResponse.NumberOfRecords != 0)
-                {
                     return Ok(apiResponse);
-                }
-                return NotFound(apiResponse);
             }
             catch
             {
@@ -83,17 +67,13 @@
             }
         }
 
-        [HttpGet("search")]
+        [HttpPost("search")]
         public async Task<IActionResult> searchPost([FromForm] string search, [FromQuery] int page = 1)
         {
             try
             {
                 var apiResponse = await _postRepository.searchPostAsync(search, page);
-                if (apiResponse.NumberOfRecords != 0)
-                {
                     return Ok(apiResponse);
-                }
-                return NotFound(apiResponse);
             }
             catch
             {
@@ -102,7 +82,7 @@
         }
 
         [HttpPost("create")]
-        public async Task<IActionResult> createPost([FromForm] string token, [FromForm] PostVM postVM)
+        public async Task<IActionResult> createPost([FromForm] int userId, [FromForm] PostVM postVM)
         {
             try
             {
@@ -110,19 +90,8 @@
                 {
                     return BadRequest(new ApiMessage { Message = Message.CREATE_FAILED.ToString() });
                 }
-                var apiResponse = await _postRepository.createPostAsync(token, postVM);
-                if (apiResponse.NumberOfRecords != 0)
-                {
+                var apiResponse = await _postRepository.createPostAsync(userId, postVM);
                     return Ok(apiResponse);
-                }
-                else
-                {
-                    if (apiResponse.Message == Message.NOT_YET_LOGIN.ToString())
-                    {
-                        return BadRequest(apiResponse);
-                    }
-                    return NotFound(apiResponse);
-                }
             }
             catch
             {
@@ -131,7 +100,7 @@
         }
 
         [HttpPost("comment/create/{id}")]
-        public async Task<IActionResult> commentPost([FromForm] string token, [FromRoute] int id, [FromForm] CommentVM commentVM)
+        public async Task<IActionResult> commentPost([FromForm] int userId, [FromRoute] int id, [FromForm] CommentVM commentVM)
         {
             try
             {
@@ -139,16 +108,8 @@
                 {
                     return BadRequest(new ApiMessage { Message = Message.FAILED.ToString() });
                 }
-                var apiMessage = await _postRepository.commentPostAsync(token, id, commentVM);
-                if (apiMessage.Message == Message.SUCCESS.ToString())
-                {
+                var apiMessage = await _postRepository.commentPostAsync(userId, id, commentVM);
                     return Ok(apiMessage);
-                }
-                if (apiMessage.Message == Message.NOT_YET_LOGIN.ToString())
-                {
-                    return BadRequest(apiMessage);
-                }
-                return NotFound(apiMessage);
             }
             catch
             {
@@ -162,11 +123,7 @@
             try
             {
                 var apiMessage = await _postRepository.hidePostAsync(id);
-                if (apiMessage.Message == Message.SUCCESS.ToString())
-                {
                     return Ok(apiMessage);
-                }
-                return NotFound(apiMessage);
             }
             catch
             {
@@ -180,11 +137,7 @@
             try
             {
                 var apiMessage = await _postRepository.showPostAsync(id);
-                if (apiMessage.Message == Message.SUCCESS.ToString())
-                {
                     return Ok(apiMessage);
-                }
-                return NotFound(apiMessage);
             }
             catch
             {
--- BTP_API/ServicesImpl/PostRepository.cs
+++ BTP_API/ServicesImpl/PostRepository.cs
@@ -13,13 +13,6 @@
         public async Task<ApiResponse> get3PostAsync()
         {
             var posts = await _context.Posts.Include(p => p.User).Where(p => p.Status == StatusRequest.Approved.ToString() && p.IsHide == false).OrderByDescending(p => p.Id).Take(3).ToListAsync();
-            if (posts.Count == 0)
-            {
-                return new ApiResponse
-                {
-                    Message = Message.LIST_EMPTY.ToString()
-                };
-            }
             return new ApiResponse
             {
                 Message = Message.GET_SUCCESS.ToString(),
@@ -30,64 +23,36 @@
 
         public async Task<ApiResponse> getAllPostAsync(int page = 1)
         {
-            var posts = await _context.Posts.Include(p => p.User).Where(p => p.Status == StatusRequest.Approved.ToString() && p.IsHide == false).ToListAsync();
-            if (posts.Count == 0)
-            {
-                return new ApiResponse
-                {
-                    Message = Message.LIST_EMPTY.ToString()
-                };
-            }
-            var result = PaginatedList<Post>.Create(posts, page, 10);
+            var posts = await _context.Posts.Include(p => p.User).Where(p => p.Status == StatusRequest.Approved.ToString() && p.IsHide == false).OrderByDescending(p => p.Id).Skip(10*(page-1)).Take(10).ToListAsync();
+            var count = await _context.Posts.Where(p => p.Status == StatusRequest.Approved.ToString() && p.IsHide == false).CountAsync();
+            //var result = PaginatedList<Post>.Create(posts, page, 10);
             return new ApiResponse
             {
                 Message = Message.GET_SUCCESS.ToString(),
-                Data = result,
-                NumberOfRecords = result.Count
+                Data = posts,
+                NumberOfRecords = count
             };
         }
 
         public async Task<ApiResponse> getPostByIdAsync(int postId)
         {
             var post = await _context.Posts.Include(p => p.User).SingleOrDefaultAsync(p => p.Id == postId && p.Status == StatusRequest.Approved.ToString() && p.IsHide == false);
-            if (post == null)
-            {
-                return new ApiResponse
-                {
-                    Message = Message.POST_NOT_EXIST.ToString()
-                };
-            }
             return new ApiResponse
             {
                 Message = Message.GET_SUCCESS.ToString(),
                 Data = post,
-                NumberOfRecords = 1
             };
         }
         public async Task<ApiResponse> getCommentInPostAsync(int postId, int page = 1)
         {
-            var check = await _context.Posts.AnyAsync(p => p.Id == postId && p.Status == StatusRequest.Approved.ToString() && p.IsHide == false);
-            if (!check)
-            {
-                return new ApiResponse
-                {
-                    Message = Message.POST_NOT_EXIST.ToString()
-                };
-            }
-            var comments = await _context.Comments.Include(p => p.User).Where(p => p.PostId == postId).ToListAsync();
-            if (comments.Count == 0)
-            {
-                return new ApiResponse
-                {
-                    Message = Message.LIST_EMPTY.ToString()
-                };
-            }
-            var result = PaginatedList<Comment>.Create(comments, page, 3);
+            var comments = await _context.Comments.Include(p => p.User).Where(p => p.PostId == postId).OrderByDescending(p => p.Id).Skip(5 * (page - 1)).Take(5).ToListAsync();
+            var count = await _context.Comments.Where(p => p.PostId == postId).CountAsync();
+            //var result = PaginatedList<Comment>.Create(comments, page, 3);
             return new ApiResponse
             {
                 Message = Message.GET_SUCCESS.ToString(),
-                Data = result,
-                NumberOfRecords = result.Count
+                Data = comments,
+                NumberOfRecords = count
             };
         }
         public async Task<ApiResponse> searchPostAsync(string search, int page = 1)
@@ -96,42 +61,26 @@
             if (search != null)
             {
                 search = search.ToLower().Trim();
-                posts = await _context.Posts.Include(b => b.User).Where(b =>  b.Title.ToLower().Contains(search) && b.Status == StatusRequest.Approved.ToString() && b.IsHide == false).ToListAsync();
+                posts = await _context.Posts.Include(b => b.User).Where(b =>  b.Title.ToLower().Contains(search) && b.Status == StatusRequest.Approved.ToString() && b.IsHide == false).OrderByDescending(b => b.Id).ToListAsync();
             }
             else
             {
-                posts = await _context.Posts.Include(p => p.User).Where(p => p.Status == StatusRequest.Approved.ToString() && p.IsHide == false).ToListAsync();
+                posts = await _context.Posts.Include(p => p.User).Where(p => p.Status == StatusRequest.Approved.ToString() && p.IsHide == false).OrderByDescending(b => b.Id).ToListAsync();
             }
-            
-            if (posts.Count == 0)
-            {
-                return new ApiResponse
-                {
-                    Message = Message.LIST_EMPTY.ToString()
-                };
-            }
-            var result = PaginatedList<Post>.Create(posts, page, 10);
+
+            //var result = PaginatedList<Post>.Create(posts, page, 10);
             return new ApiResponse
             {
                 Message = Message.GET_SUCCESS.ToString(),
-                Data = result,
-                NumberOfRecords = result.Count
+                Data = posts.Skip(10 * (page - 1)).Take(10),
+                NumberOfRecords = posts.Count
             };
         }
-        public async Task<ApiResponse> createPostAsync(string token, PostVM postVM)
+        public async Task<ApiResponse> createPostAsync(int userId, PostVM postVM)
         {
             UploadFile uploadFile = new UploadFile();
-            Cookie cookie = new Cookie();
             string fileImageName = uploadFile.UploadPostImage(postVM, _environment);
 
-            var userId = cookie.GetUserId(token);
-            if (userId == 0)
-            {
-                return new ApiResponse
-                {
-                    Message = Message.NOT_YET_LOGIN.ToString()
-                };
-            }
             var post = new Post
             {
                 UserId = userId,
@@ -152,14 +101,8 @@
                 NumberOfRecords = 1
             };
         }
-        public async Task<ApiMessage> commentPostAsync(string token, int postId, CommentVM commentVM)
+        public async Task<ApiMessage> commentPostAsync(int userId, int postId, CommentVM commentVM)
         {
-            Cookie cookie = new Cookie();
-            int userId = cookie.GetUserId(token);
-            if (userId == 0)
-            {
-                return new ApiMessage { Message = Message.NOT_YET_LOGIN.ToString() };
-            }
             var post = await _context.Posts.SingleOrDefaultAsync(p => p.Id == postId && p.Status == StatusRequest.Approved.ToString() && p.IsHide == false);
             if (post != null)
             {
