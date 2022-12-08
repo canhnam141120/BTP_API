c11d646d040c42a316d5c7b818f73535f17994f8
namnc <namnche141707@fpt.edu.vn>
2022-12-09 03:15:51 +0700

    Dp

--- BTP_API/ServicesImpl/BookRepository.cs
+++ BTP_API/ServicesImpl/BookRepository.cs
@@ -500,11 +500,13 @@ namespace BTP_API.Services
             {
                 search = search.ToLower().Trim();
 
+                search = search.RemoveAccents();
+
                 var listStr = search.Split(' ');
 
                 foreach (var s in listStr)
                 {
-                    var bookResult = await _context.Books.Include(b => b.User).Include(b => b.Category).Where(b => b.Title.ToLower().Contains(s) && b.IsReady == true && b.Status == StatusRequest.Approved.ToString()).ToListAsync();
+                    var bookResult = await _context.Books.Include(b => b.User).Include(b => b.Category).Where(b => b.Title.ToLower().RemoveAccents().Contains(s) && b.IsReady == true && b.Status == StatusRequest.Approved.ToString()).ToListAsync();
                     books.AddRange(bookResult);
                     books.OrderBy(b => b.Title);
                 }
