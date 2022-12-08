336797f84c7db33d40909beb9e030bd5fb0a2d00
namnc <namnche141707@fpt.edu.vn>
2022-12-09 04:10:53 +0700

    Deploy7 search

---
+++ BTP_API/Helpers/StringExtension.cs
@@ -0,0 +1,48 @@
+﻿namespace BTP_API.Helpers
+{
+    public static class StringExtension
+    {
+        private static readonly string[] VietnameseSigns = new string[]
+        {
+        "aAeEoOuUiIdDyY",
+        "áàạảãâấầậẩẫăắằặẳẵ",
+        "ÁÀẠẢÃÂẤẦẬẨẪĂẮẰẶẲẴ",
+        "éèẹẻẽêếềệểễ",
+        "ÉÈẸẺẼÊẾỀỆỂỄ",
+        "óòọỏõôốồộổỗơớờợởỡ",
+        "ÓÒỌỎÕÔỐỒỘỔỖƠỚỜỢỞỠ",
+        "úùụủũưứừựửữ",
+        "ÚÙỤỦŨƯỨỪỰỬỮ",
+        "íìịỉĩ",
+        "ÍÌỊỈĨ",
+        "đ",
+        "Đ",
+        "ýỳỵỷỹ",
+        "ÝỲỴỶỸ"
+        };
+
+        public static string RemoveAccents(this string text)
+        {
+            if (String.IsNullOrEmpty(text))
+            {
+                return string.Empty;
+            }
+
+            for (int i = 1; i < VietnameseSigns.Length; i++)
+            {
+                for (int j = 0; j < VietnameseSigns[i].Length; j++)
+                    text = text.Replace(VietnameseSigns[i][j], VietnameseSigns[0][i - 1]);
+            }
+            return text;
+        }
+
+        public static string Standardizing(this string text)
+        {
+            if (String.IsNullOrEmpty(text))
+            {
+                return string.Empty;
+            }
+            return text.RemoveAccents().ToLower();
+        }
+    }
+}
